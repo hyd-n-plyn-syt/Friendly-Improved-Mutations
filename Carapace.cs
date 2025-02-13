efb69e8f3e433c2f3bf4addea15fc941a3ed857d
hyd-n-plyn-syt <hydnplynsyt@gmail.com>
2025-02-13 16:21:22 -0500

    BurrowingClaws.cs * Removed. Outdated, changes are in the base game. Carapace.cs, Horns.cs, HornsProperties.cs, Improved_Wings.cs * Removed commented out assembly language. Tidied. ObjectBlueprints.xml * Removed unused entries from removed mutations. Snake Tail & Fangs.

--- Carapace.cs
+++ Carapace.cs
@@ -6,36 +6,30 @@ using XRL.World.Anatomy;
 
 namespace XRL.World.Parts.Mutation
 {
-    // Token: 0x02000F7D RID: 3965
     [Serializable]
     public class Carapace : BaseDefaultEquipmentMutation
     {
-        // Token: 0x060099C4 RID: 39364 RVA: 0x003B3F90 File Offset: 0x003B2190
         public override IPart DeepCopy(GameObject Parent, Func<GameObject, GameObject> MapInv)
         {
             Carapace carapace = base.DeepCopy(Parent, MapInv) as Carapace;
             return carapace;
         }
 
-        // Token: 0x060099C5 RID: 39365 RVA: 0x003B3FA6 File Offset: 0x003B21A6
         public Carapace()
         {
             this.DisplayName = "Carapace";
         }
 
-        // Token: 0x060099C6 RID: 39366 RVA: 0x003B3FC4 File Offset: 0x003B21C4
         public override bool GeneratesEquipment()
         {
             return true;
         }
 
-        // Token: 0x060099C7 RID: 39367 RVA: 0x003B3FC7 File Offset: 0x003B21C7
         public override bool WantEvent(int ID, int cascade)
         {
             return base.WantEvent(ID, cascade) || ID == AIGetDefensiveAbilityListEvent.ID || ID == GetEnergyCostEvent.ID;
         }
 
-        // Token: 0x060099C8 RID: 39368 RVA: 0x003B3FE8 File Offset: 0x003B21E8
         public override bool HandleEvent(AIGetDefensiveAbilityListEvent E)
         {
             int high = Math.Max(this.ParentObject.baseHitpoints - E.Distance, 1);
@@ -46,7 +40,6 @@ namespace XRL.World.Parts.Mutation
             return base.HandleEvent(E);
         }
 
-        // Token: 0x060099C9 RID: 39369 RVA: 0x003B406C File Offset: 0x003B226C
         public override bool HandleEvent(GetEnergyCostEvent E)
         {
             if (this.Tight && (E.Type == null || (!E.Type.Contains("Pass") && !E.Type.Contains("Mental") && !E.Type.Contains("Carapace"))))
@@ -64,7 +57,6 @@ namespace XRL.World.Parts.Mutation
             return base.HandleEvent(E);
         }
 
-        // Token: 0x060099CA RID: 39370 RVA: 0x003B4144 File Offset: 0x003B2344
         public override void Register(GameObject Object)
         {
             Object.RegisterPartEvent(this, "BeginMove");
@@ -73,19 +65,16 @@ namespace XRL.World.Parts.Mutation
             base.Register(Object);
         }
 
-        // Token: 0x060099CB RID: 39371 RVA: 0x003B4171 File Offset: 0x003B2371
         public override string GetDescription()
         {
             return "You are protected by a durable carapace.";
         }
 
-        // Token: 0x060099CC RID: 39372 RVA: 0x003B4178 File Offset: 0x003B2378
         public override string GetLevelText(int Level)
         {
             return "+{{C|" + (3 + (int)(Level / 2)) + "}} AV\n" + "-2 DV\n" + "+{{C|" + (5 + 5 * Level).ToString() + "}} Heat Resistance\n" + "+{{C|" + (5 + 5 * Level).ToString() + "}} Cold Resistance\n" + "You may tighten your carapace to receive double the AV bonus at a -2 DV penalty as long as you remain still.\n" + "Cannot wear body armor.\n" + "+400 reputation with {{w|tortoises}}";
         }
 
-        // Token: 0x060099CD RID: 39373 RVA: 0x003B4210 File Offset: 0x003B2410
         public override bool FireEvent(Event E)
         {
             if (E.ID == "CommandTightenCarapace")
@@ -127,7 +116,6 @@ namespace XRL.World.Parts.Mutation
             return base.FireEvent(E);
         }
 
-        // Token: 0x060099CE RID: 39374 RVA: 0x003B43A8 File Offset: 0x003B25A8
         public void Tighten()
         {
             if (!this.Tight)
@@ -139,7 +127,6 @@ namespace XRL.World.Parts.Mutation
             }
         }
 
-        // Token: 0x060099CF RID: 39375 RVA: 0x003B441C File Offset: 0x003B261C
         public void Loosen()
         {
             if (this.Tight)
@@ -151,7 +138,6 @@ namespace XRL.World.Parts.Mutation
             }
         }
 
-        // Token: 0x060099D0 RID: 39376 RVA: 0x003B4488 File Offset: 0x003B2688
         public override bool ChangeLevel(int NewLevel)
         {
             this.Loosen();
@@ -190,7 +176,6 @@ namespace XRL.World.Parts.Mutation
             return base.ChangeLevel(NewLevel);
         }
 
-        // Token: 0x060099D1 RID: 39377 RVA: 0x003B45E3 File Offset: 0x003B27E3
         public override void OnRegenerateDefaultEquipment(Body body)
         {
             foreach (BodyPart torso in body.GetPart("Body"))
@@ -199,7 +184,6 @@ namespace XRL.World.Parts.Mutation
             }
         }
 
-        // Token: 0x060099D2 RID: 39378 RVA: 0x003B45F8 File Offset: 0x003B27F8
         public void AddCarapaceTo(BodyPart body)
         {
             if (body == null)
@@ -215,7 +199,6 @@ namespace XRL.World.Parts.Mutation
             body.ParentBody.ParentObject.ForceEquipObject(theCarapace, body, true, new int?(0));
         }
 
-        // Token: 0x060099D3 RID: 39379 RVA: 0x003B465C File Offset: 0x003B285C
         public override bool Mutate(GameObject GO, int Level)
         {
             Body body = GO.Body;
@@ -225,12 +208,11 @@ namespace XRL.World.Parts.Mutation
                 {
                     this.AddCarapaceTo(torso);
                 }
-                this.ActivatedAbilityID = base.AddMyActivatedAbility("Tighten " + this.DisplayName, "CommandTightenCarapace", "Physical Mutation", null, "ï", null, false, false, false, false, false, false, false, false, true, true, false, -1, null, null, null, null, null, null);
+                this.ActivatedAbilityID = base.AddMyActivatedAbility("Tighten " + this.DisplayName, "CommandTightenCarapace", "Physical Mutation", null, "ï¿½", null, false, false, false, false, false, false, false, false, true, true, false, -1, null, null, null, null, null, null);
             }
             return base.Mutate(GO, Level);
         }
 
-        // Token: 0x060099D4 RID: 39380 RVA: 0x003B46C4 File Offset: 0x003B28C4
         public override bool Unmutate(GameObject GO)
         {
             this.Loosen();
@@ -251,24 +233,18 @@ namespace XRL.World.Parts.Mutation
                 GameObject carapaceToRemove = torso.Equipped;
                 base.CleanUpMutationEquipment(GO, ref carapaceToRemove);
             }
-            //base.CleanUpMutationEquipment(GO, ref this.CarapaceObject);
             base.RemoveMyActivatedAbility(ref this.ActivatedAbilityID, null);
             return base.Unmutate(GO);
         }
 
-        // Token: 0x04003E3B RID: 15931
         public int ACModifier;
 
-        // Token: 0x04003E3C RID: 15932
         public int DVModifier;
 
-        // Token: 0x04003E3D RID: 15933
         public int ResistanceMod;
 
-        // Token: 0x04003E3E RID: 15934
         public bool Tight;
 
-        // Token: 0x04003E3F RID: 15935
         public int TightFactor;
     }
 }
--- Horns.cs
+++ Horns.cs
@@ -6,36 +6,30 @@ using XRL.World.Anatomy;
 
 namespace XRL.World.Parts.Mutation
 {
-	// Token: 0x02000F83 RID: 3971
 	[Serializable]
 	public class Horns : BaseMutation
 	{
-		// Token: 0x060099AC RID: 39340 RVA: 0x003B4354 File Offset: 0x003B2554
 		public Horns()
 		{
 			this.DisplayName = "Horns";
 		}
 
-		// Token: 0x060099AD RID: 39341 RVA: 0x003B43B4 File Offset: 0x003B25B4
 		public override bool GeneratesEquipment()
 		{
 			return true;
 		}
 
-		// Token: 0x060099AE RID: 39342 RVA: 0x003B43B7 File Offset: 0x003B25B7
 		public override bool WantEvent(int ID, int cascade)
 		{
 			return base.WantEvent(ID, cascade) || ID == RegenerateDefaultEquipmentEvent.ID;
 		}
 
-		// Token: 0x060099AF RID: 39343 RVA: 0x003B43CD File Offset: 0x003B25CD
 		public override bool HandleEvent(RegenerateDefaultEquipmentEvent E)
 		{
 			this.RegrowHorns();
 			return base.HandleEvent(E);
 		}
 
-		// Token: 0x060099B0 RID: 39344 RVA: 0x003B43E0 File Offset: 0x003B25E0
 		public override string GetDescription()
         {
             if (this.Variant == null)
@@ -67,26 +61,22 @@ namespace XRL.World.Parts.Mutation
             return Grammar.A(cachedDisplayNameStripped, true) + " juts out of your heads.";
         }
 
-		// Token: 0x060099B1 RID: 39345 RVA: 0x003B444A File Offset: 0x003B264A
 		public int GetAV(int Level)
 		{
 			return 1 + (Level - 1) / 3;
 		}
 
-		// Token: 0x060099B2 RID: 39346 RVA: 0x003B4454 File Offset: 0x003B2654
 		public string GetBaseDamage(int Level)
 		{
 			return "2d" + (3 + Level / 2).ToString();
 		}
 
-		// Token: 0x060099B3 RID: 39347 RVA: 0x003B4478 File Offset: 0x003B2678
 		public override IPart DeepCopy(GameObject Parent, Func<GameObject, GameObject> MapInv)
 		{
 			Horns horns = base.DeepCopy(Parent, MapInv) as Horns;
 			return horns;
 		}
 
-		// Token: 0x060099B4 RID: 39348 RVA: 0x003B4490 File Offset: 0x003B2690
 		public override string GetLevelText(int Level)
 		{
 			string baseDamage = this.GetBaseDamage(Level);
@@ -126,7 +116,6 @@ namespace XRL.World.Parts.Mutation
             return str + "+100 reputation with {{w|antelopes}} and {{w|goatfolk}}";
 		}
 
-        // Token: 0x0600969B RID: 38555 RVA: 0x003A308C File Offset: 0x003A128C
         private void RegrowHorns()
         {
             Body body = this.ParentObject.Body;
@@ -168,14 +157,12 @@ namespace XRL.World.Parts.Mutation
             Mutating = false;
         }
 
-		// Token: 0x060099B6 RID: 39350 RVA: 0x003B46A3 File Offset: 0x003B28A3
 		public override bool ChangeLevel(int NewLevel)
 		{
 			this.RegrowHorns();
 			return base.ChangeLevel(NewLevel);
 		}
 
-		// Token: 0x060099B8 RID: 39352 RVA: 0x003B46BC File Offset: 0x003B28BC
 		public override void SetVariant(string Variant)
 		{
             base.SetVariant(Variant);
@@ -185,7 +172,6 @@ namespace XRL.World.Parts.Mutation
             }
 		}
 
-		// Token: 0x060099B9 RID: 39353 RVA: 0x003B4714 File Offset: 0x003B2914
 		public override bool Mutate(GameObject GO, int Level)
 		{
             Mutating = true;
@@ -197,7 +183,6 @@ namespace XRL.World.Parts.Mutation
             return base.Mutate(GO, Level);
 		}
 
-		// Token: 0x060099BA RID: 39354 RVA: 0x003B47FA File Offset: 0x003B29FA
 		public override bool Unmutate(GameObject GO)
 		{
 			foreach (BodyPart Head in GO.Body.GetPart("Head"))
@@ -208,17 +193,14 @@ namespace XRL.World.Parts.Mutation
 			return base.Unmutate(GO);
 		}
 
-		// Token: 0x04003E13 RID: 15891
 		public bool Mutating = false;
 
-		// Token: 0x04003E14 RID: 15892
 		public string HornsName;
 		
 		public GameObject HornsObject;
         
         public int HeadsAmount = 1;
 
-		// Token: 0x04003E15 RID: 15893
 		[NonSerialized]
 		private List<string> variants = new List<string>
 		{
